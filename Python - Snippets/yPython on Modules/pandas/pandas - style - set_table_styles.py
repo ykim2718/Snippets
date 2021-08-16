9013cca9cd4c373d6b3230a8c520a964596c441b
y <ykim7389@naver.com>
2021-08-16 10:10:51 +0900

    y

--- Python - Snippets/yPython on Modules/pandas/pandas - style - set_table_styles.py
+++ Python - Snippets/yPython on Modules/pandas/pandas - style - set_table_styles.py
@@ -7,11 +7,13 @@ https://pandas.pydata.org/docs/reference/api/pandas.io.formats.style.Styler.set_
 import pandas as pd
 import pathlib
 
-a = pd.DataFrame(dict(a=[1, 2], b=[3, 4]))
+a = pd.DataFrame(dict(a=[1, 2], b=[3, 4], c=[pd.Timestamp.now()] * 2))
 
 # FIXME !!! # html = self.styler.set_td_classes(classes=["table-bordered", "table-striped", "table-hover"]).render()
 
-a = a.style.set_table_styles([{'selector': 'tr', 'props': 'background-color: yellow; font-size: 1em;'}])
+a = a.style\
+    .set_table_styles([{'selector': 'tr', 'props': 'background-color: yellow; font-size: 1em;'}])\
+    .format(({'c': "{:%y-%m-%d}"}))
 html = a.render()
 
 pathlib.Path(__file__).with_suffix('.html').write_text(html)
