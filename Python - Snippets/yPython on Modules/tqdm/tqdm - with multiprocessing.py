6ec342a5731f77e5dee3d2f01489dd8b49d19017
y <ykim7389@naver.com>
2021-09-19 09:40:49 +0900

    y

---
+++ Python - Snippets/yPython on Modules/tqdm/tqdm - with multiprocessing.py
@@ -0,0 +1,38 @@
+"""
+y, 2021.9.19
+tqdm - with multiprocessing.py
+https://github.com/tqdm/tqdm/issues/484
+"""
+
+import time
+import random
+from multiprocessing import Pool
+from tqdm import tqdm
+
+
+def myfunc(a):
+    time.sleep(random.random())
+    return a ** 2
+
+
+if __name__ == '__main__':
+    # freeze_support()
+
+    pool = Pool(2)
+    '''
+    for _ in tqdm(pool.imap_unordered(myfunc, range(100)), total=100):
+        pass
+    '''
+    pbar = tqdm(total=100)
+
+
+    def update(*a):
+        pbar.update()
+        # tqdm.write(str(a))
+
+
+    for i in range(pbar.total):
+        pool.apply_async(myfunc, args=(i,), callback=update)
+    # tqdm.write('scheduled')
+    pool.close()
+    pool.join()
