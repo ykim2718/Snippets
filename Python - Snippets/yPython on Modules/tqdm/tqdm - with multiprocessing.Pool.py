3b9084bcb3ce3fef727c0ff71bacad69dee0d72a
y <ykim7389@naver.com>
2021-09-21 14:31:21 +0900

    y

--- Python - Snippets/yPython on Modules/tqdm/tqdm - with multiprocessing.Pool.py
+++ Python - Snippets/yPython on Modules/tqdm/tqdm - with multiprocessing.Pool.py
@@ -1,5 +1,5 @@
 """
-y, 2021.9.19
+y, 2021.9.19, 9.21 (추석날)
 tqdm - with multiprocessing.Pool.py
 https://github.com/tqdm/tqdm/issues/484
 https://stackoverflow.com/questions/41920124/multiprocessing-use-tqdm-to-display-a-progress-bar
@@ -16,24 +16,25 @@ def myfunc(a):
     return a ** 2
 
 
+def update(*a):
+    pbar.update()
+    # tqdm.write(str(a))
+
+
 if __name__ == '__main__':
     # freeze_support()
 
     pool = Pool(2)
-    '''
-    for _ in tqdm(pool.imap_unordered(myfunc, range(100)), total=100):
-        pass
-    '''
-    pbar = tqdm(total=100)
 
+    if False:
+        for _ in tqdm(pool.imap_unordered(myfunc, range(100)), total=100):
+            pass
 
-    def update(*a):
-        pbar.update()
-        # tqdm.write(str(a))
+    else:
+        pbar = tqdm(total=100)
+        for i in range(pbar.total):
+            pool.apply_async(myfunc, args=(i,), callback=update)
 
-
-    for i in range(pbar.total):
-        pool.apply_async(myfunc, args=(i,), callback=update)
     # tqdm.write('scheduled')
     pool.close()
     pool.join()
