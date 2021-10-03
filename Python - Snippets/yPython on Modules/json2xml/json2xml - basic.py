b2a8f6587d34569a2b44423d5e3144d17c06b995
y <ykim7389@naver.com>
2021-10-03 23:44:41 +0900

    y

---
+++ Python - Snippets/yPython on Modules/json2xml/json2xml - basic.py
@@ -0,0 +1,24 @@
+"""
+y, 2021.10.3
+json2xml - basic.py
+https://pypi.org/project/json2xml/
+"""
+
+from json2xml import json2xml
+from json2xml.utils import readfromurl, readfromstring, readfromjson
+
+# get the xml from an URL that return json
+if False:
+    data = readfromurl("https://coderwall.com/vinitcool76.json")
+    print(json2xml.Json2xml(data).to_xml())
+
+# get the xml from a json string
+data = readfromstring(
+    '{"login":"mojombo","id":1,"avatar_url":"https://avatars0.githubusercontent.com/u/1?v=4"}'
+)
+print(json2xml.Json2xml(data).to_xml())
+
+# get the data from an URL
+if False:
+    data = readfromjson("examples/licht.json")
+    print(json2xml.Json2xml(data).to_xml())
