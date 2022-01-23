ea728c1a709fab91d01e0f6ed0a3cb348446a0d1
y <ykim7389@naver.com>
2022-01-23 19:59:17 +0900

    y

--- Python - Snippets/yPython on Modules/pymongo/pymongo - rs.status().py
+++ Python - Snippets/yPython on Modules/pymongo/pymongo - rs.status().py
@@ -8,7 +8,7 @@ import pymongo
 import time
 from bson.codec_options import DEFAULT_CODEC_OPTIONS
 
-code_options = DEFAULT_CODEC_OPTIONS.with_options(unicode_decode_error_handler='ignore')
+codec_options = DEFAULT_CODEC_OPTIONS.with_options(unicode_decode_error_handler='ignore')
 servers = dict(
     S1='10.227.253.174',
     S2='10.227.253.148',
@@ -21,15 +21,16 @@ replicas = dict(
     PASystem_mongodb_shard_s3=28003,
 )
 
+server = servers['S1']
 for key, value in replicas.items():
-    # uri = f"mongodb://{servers['S2']}:{value}/?replicaSet={key}"
-    uri = f"mongodb://{servers['S2']}:{value}"
+    # uri = f"mongodb://{server}:{value}/?replicaSet={key}"
+    uri = f"mongodb://{server}:{value}"
     print(f"replica={key}")
     print(f"uri={uri}")
     client = pymongo.MongoClient(uri, replicaset=key)
     time.sleep(0.1)
     print(f"client.nodes={client.nodes}")
-    status = client.admin.command('replSetGetStatus', codec_options=code_options)
-    # status = client['admin'].command({'replSetGetStatus': 1}, codec_options=code_options)
+    status = client.admin.command('replSetGetStatus', codec_options=codec_options)
+    # status = client['admin'].command({'replSetGetStatus': 1}, codec_options=codec_options)
     for member in status['members']:
         print('\t', member['name'], member['stateStr'])
