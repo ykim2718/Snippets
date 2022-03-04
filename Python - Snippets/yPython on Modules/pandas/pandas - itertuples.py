714c65c5d7e31c5bd7a6586bfd6d9c58cbbf562e
Y <ykim7389@naver.com>
2022-03-04 21:19:11 +0900

    y desktop

---
+++ Python - Snippets/yPython on Modules/pandas/pandas - itertuples.py
@@ -0,0 +1,15 @@
+"""
+y, 2022.3.4
+pandas - itertuples.py
+https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.itertuples.html
+"""
+
+import pandas as pd
+
+df = df = pd.DataFrame({'num_legs': [4, 2], 'num_wings': [0, 2]}, index=['dog', 'hawk'])
+
+for row in df.itertuples():
+    print(row, row.Index, row.num_legs, row.num_wings)
+
+for row in df.itertuples(name='Animal'):
+    print(row)
