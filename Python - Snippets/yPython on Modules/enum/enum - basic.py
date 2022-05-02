d0e0c2a3c0f40c2cf627b592c422d2a7bd7068d5
Y <ykim7389@naver.com>
2022-05-03 00:47:25 +0900

    y desktop

--- Python - Snippets/yPython on Modules/enum/enum - basic.py
+++ Python - Snippets/yPython on Modules/enum/enum - basic.py
@@ -11,10 +11,16 @@ class Color(Enum):
     RED = 1
     GREEN = 2
     BLUE = 3
+    PINK = 'pink'
 
 
 print(Color.RED)  # Color.RED
-print(repr(Color.RED))  # <Color.RED: 1
+print(Color.RED.name)  # 'RED'
+print(Color.RED.value)  # 1
+print(str(Color.RED))  # 'Color.RED'
+print(repr(Color.RED))  # <Color.RED: 1>
 print(type(Color.RED))  # <enum 'Color'>
 print(Color.RED == 1)  # FALSE
-print(list(Color))  # [<Color.RED: 1>, <Color.GREEN: 2>, <Color.BLUE: 3>]
+print(list(Color))  # [<Color.RED: 1>, <Color.GREEN: 2>, <Color.BLUE: 3>, <Color.PINK: 'pink'>]
+print([e.name for e in Color])  # ['RED', 'GREEN', 'BLUE', 'PINK']
+print(Color.RED in Color)  # True
