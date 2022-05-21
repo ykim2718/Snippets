9554a020b59a0a4445ce9690f5918dc37e24e4f0
Y <ykim7389@naver.com>
2022-05-21 18:01:06 +0900

    y desktop

--- Python - Snippets/yPython on Modules/heapq/heapq - lazy remove.py
+++ Python - Snippets/yPython on Modules/heapq/heapq - lazy remove.py
@@ -1,5 +1,5 @@
 """
-y, 2022.5.19 - 20
+y, 2022.5.19 - 21
 heapq - lazy remove.py
 """
 
@@ -22,22 +22,27 @@ class HeapNode:
 
 class LazyHeap:
     def __init__(self):
-        self._pq = []  # prioriy queue
+        self._pq = []  # priority queue
         self._flags = dict()  # use integer key for speed
 
-    def push(self, a):
+    def push(self, a, data=True):
         assert hasattr(a, 'index') and isinstance(a.index, int)
         assert hasattr(a, 'value')
         hq.heappush(self._pq, a)
-        self._flags[a.index] = True
+        self._flags[a.index] = data
 
     def pop(self):
-        while self._pq and not self._flags.get(self._pq[0].index, False):
+        while self._pq and self._pq[0].index not in self._flags:
             hq.heappop(self._pq)
         return hq.heappop(self._pq) if self._pq else None
 
     def remove(self, index):
-        self._flags[index] = False
+        assert isinstance(index, int)
+        self._flags.pop(index, None)
+
+    def get(self, index, default=None):
+        assert isinstance(index, int)
+        return self._flags.get(index, default)
 
 
 lh = LazyHeap()
@@ -56,6 +61,7 @@ print(f"{lh.pop()=}, {lh._pq=}")
 print(f"{lh.pop()=}, {lh._pq=}")
 print(f"{lh.pop()=}, {lh._pq=}")
 print(f"{lh.pop()=}, {lh._pq=}")
+print(f"{lh.get(8)=}")
 print(f"{lh.pop()=}, {lh._pq=}")
 print(f"{lh.pop()=}, {lh._pq=}")
 """
@@ -69,12 +75,13 @@ lh._pq=[(3, 30), (6, 60), (4, 40), (7, 70), (8, 80), (9, 90), (5, 50)]
 lh._pq=[(2, 20), (3, 30), (4, 40), (6, 60), (8, 80), (9, 90), (5, 50), (7, 70)]
 lh._pq=[(1, 10), (2, 20), (4, 40), (3, 30), (8, 80), (9, 90), (5, 50), (7, 70), (6, 60)]
 lh._pq=[(0, 0), (1, 10), (4, 40), (3, 30), (2, 20), (9, 90), (5, 50), (7, 70), (6, 60), (8, 80)]
-lh._flags={6: True, 5: False, 4: True, 7: False, 8: True, 9: True, 3: False, 2: True, 1: False, 0: False}
+lh._flags={6: True, 4: True, 8: True, 9: True, 2: True}}
 lh._pq=[(0, 0), (1, 10), (4, 40), (3, 30), (2, 20), (9, 90), (5, 50), (7, 70), (6, 60), (8, 80)]
 lh.pop()=(2, 20), lh._pq=[(3, 30), (6, 60), (4, 40), (7, 70), (8, 80), (9, 90), (5, 50)]
 lh.pop()=(4, 40), lh._pq=[(5, 50), (6, 60), (9, 90), (7, 70), (8, 80)]
 lh.pop()=(6, 60), lh._pq=[(7, 70), (8, 80), (9, 90)]
 lh.pop()=(8, 80), lh._pq=[(9, 90)]
+lh.get(8)=True
 lh.pop()=(9, 90), lh._pq=[]
 lh.pop()=None, lh._pq=[]
 """
