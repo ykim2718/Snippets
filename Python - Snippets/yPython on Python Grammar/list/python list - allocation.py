32f003db7e06e7e80969c064bf3c78d68c0d2922
Y <ykim7389@naver.com>
2022-05-27 21:13:28 +0900

    y desktop

---
+++ Python - Snippets/yPython on Python Grammar/list/python list - allocation.py
@@ -0,0 +1,40 @@
+"""
+y, 2022.5.27
+python list - allocation.py
+"""
+
+import numpy as np
+
+print('- ' * 16)
+a = [0] * 2
+print(a)  # [0, 0]
+a[1] = 1
+print(a)  # [0, 1]
+
+print('- ' * 16)
+a = [[0]] * 2
+print(a)  # [[0], [0]]
+a[1][0] = 1
+print(a)  # [[1], [1]]  << CAUTIOUS
+
+print('- ' * 16)
+a = [[0] * 2] * 2
+print(a, np.array(a).shape)  # [[0, 0], [0, 0]] (2, 2)
+a[1][1] = 1
+print(a)  # [[0, 1], [0, 1]]  << CAUTIOUS
+
+print('- ' * 16)
+a = [[0] * 2 for _ in range(2)]
+print(a, np.array(a).shape)  # [[0, 0], [0, 0]] (2, 2)
+a[1][1] = 1
+print(a)  # [[0, 0], [0, 1]]
+
+print('- ' * 16)
+a = [0 for _ in range(2)] * 2
+print(a, np.array(a).shape)  # [[0], [0], [0], [0]] (4, 1)
+
+print('- ' * 16)
+a = [[0 for _ in range(2)] for _ in range(2)]
+print(a, np.array(a).shape)  # [[0, 0], [0, 0]] (2, 2)
+a[1][1] = 1
+print(a)  # [[0, 0], [0, 1]]
