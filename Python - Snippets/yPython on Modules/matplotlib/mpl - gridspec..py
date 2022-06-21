6f64a95c7918f5aa4bf8d7cbe77a114138ac4f9b
Y <ykim7389@naver.com>
2022-06-21 22:07:39 +0900

    y desktop

--- Python - Snippets/yPython on Modules/matplotlib/mpl - gridspec..py
+++ Python - Snippets/yPython on Modules/matplotlib/mpl - gridspec..py
@@ -1,15 +1,15 @@
 """
-y, 2020.2.8
+y, 2016.9.4; 2020.2.8; 2021.6.21
 mpl - gridspec.py
+http://matplotlib.org/users/gridspec.html
 https://matplotlib.org/gallery/subplots_axes_and_figures/align_labels_demo.html#sphx-glr-gallery-subplots-axes-and-figures-align-labels-demo-py
 """
 
 import matplotlib.pyplot as plt
-import numpy as np
 import matplotlib.gridspec as gridspec
 
 fig = plt.figure()
-gs = gridspec.GridSpec(2, 4)
+gs = gridspec.GridSpec(2, 4, width_ratios=[1, 2, 3, 4], height_ratios=[4, 1])
 
 ax1 = fig.add_subplot(gs[0, :])
 ax2 = fig.add_subplot(gs[1, 0])
