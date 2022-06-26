be6e2340c2b0d367c9b7d8d0099961075b676c70
Y <ykim7389@naver.com>
2022-06-26 14:15:37 +0900

    y desktop

--- Python - Snippets/yPython on Modules/enum/enum - basic.py
+++ Python - Snippets/yPython on Modules/enum/enum - basic.py
@@ -1,5 +1,5 @@
 """
-y, 2022.5.2
+y, 2022.5.2, 6.26
 enum - basic.py
 https://docs.python.org/3.7/library/enum.html
 """
@@ -14,6 +14,9 @@ class Color(Enum):
     PINK = 'pink'
 
 
+print(list(Color))  # [<Color.RED: 1>, <Color.GREEN: 2>, <Color.BLUE: 3>, <Color.PINK: 'pink'>]
+print([e.name for e in Color])  # ['RED', 'GREEN', 'BLUE', 'PINK']
+
 print(Color.RED)  # Color.RED
 print(Color.RED.name)  # 'RED'
 print(Color.RED.value)  # 1
@@ -21,6 +24,10 @@ print(str(Color.RED))  # 'Color.RED'
 print(repr(Color.RED))  # <Color.RED: 1>
 print(type(Color.RED))  # <enum 'Color'>
 print(Color.RED == 1)  # FALSE
-print(list(Color))  # [<Color.RED: 1>, <Color.GREEN: 2>, <Color.BLUE: 3>, <Color.PINK: 'pink'>]
-print([e.name for e in Color])  # ['RED', 'GREEN', 'BLUE', 'PINK']
 print(Color.RED in Color)  # True
+# print('RED' in Color, '<<<<<<<<')  # TypeError: unsupported operand type(s) for 'in': 'str' and 'EnumMeta'
+
+print(Color.PINK)  # Color.PINK
+print(Color.PINK.name)  # 'PINK'
+print(Color.PINK.value)  # pink
+# print('PINK' in Color, '<<<<<<<<')  # TypeError: unsupported operand type(s) for 'in': 'str' and 'EnumMeta'
