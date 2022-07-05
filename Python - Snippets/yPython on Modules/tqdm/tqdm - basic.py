a5d96cf825ba3eb1759d30022bac601db564a760
Y <ykim7389@naver.com>
2022-07-06 00:52:59 +0900

    y desktop

--- Python - Snippets/yPython on Modules/tqdm/tqdm - basic.py
+++ Python - Snippets/yPython on Modules/tqdm/tqdm - basic.py
@@ -1,5 +1,5 @@
 """
-y, 2020.8.25, 9.11; 2022.5.17
+y, 2020.8.25, 9.11; 2022.5.17, 7.5
 tqdm - basic.py
 https://github.com/tqdm/tqdm
 https://tqdm.github.io/docs/tqdm/
@@ -11,9 +11,15 @@ import time
 
 with tqdm.tqdm(total=100, ncols=80, desc='updating', unit='byte') as pbar:
     for j in range(10):
-        time.sleep(0.1)
         pbar.update(10)
+        time.sleep(0.1)
 
 
-for i in tqdm.tqdm(range(10), disable=True):
-    time.sleep(1)
+for i in tqdm.tqdm(range(10), desc='disable=True', disable=True):
+    time.sleep(0.1)
+
+
+pbar = tqdm.tqdm(range(10), desc='trange()')
+for i in pbar:
+    pbar.set_description(f"{pbar.n=}")
+    time.sleep(0.1)
