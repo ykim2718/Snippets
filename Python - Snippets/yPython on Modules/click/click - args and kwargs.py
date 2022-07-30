d374864271772366418d9186bbf43633174e97a9
Y <ykim7389@naver.com>
2022-07-30 17:53:29 +0900

    y desktop

--- Python - Snippets/yPython on Modules/click/click - args and kwargs.py
+++ Python - Snippets/yPython on Modules/click/click - args and kwargs.py
@@ -1,5 +1,5 @@
 """
-y, click - ystudy on kwargs.py, 2017.8.12
+y, click - args and kwargs.py, 2017.8.12
 
 """
 
@@ -11,17 +11,15 @@ import click
 @click.option('-arg1', help='')
 @click.option('-kwarg1', default='default', nargs=1, help='')
 def func(arg1, kwarg1='kwarg1'):
-
     print('main >>', arg1, kwarg1)
 
 
 def main():
     pass
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     del sys.argv[1:]
     # sys.argv += ['-arg1', '1', '-kwarg1', '2']
     sys.argv += ['-arg1', '1']
     func()
-
