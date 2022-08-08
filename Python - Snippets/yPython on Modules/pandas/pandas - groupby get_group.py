203b427f3ee3f7e1205037e04d7f6fee014048f6
Y <ykim7389@naver.com>
2022-08-08 20:18:19 +0900

    y desktop

--- Python - Snippets/yPython on Modules/pandas/pandas - groupby get_group.py
+++ Python - Snippets/yPython on Modules/pandas/pandas - groupby get_group.py
@@ -9,4 +9,4 @@ import numpy as np
 df = pd.DataFrame({'key': list('ABCABC'), 'data1': [1, 2, 3, 1, 2, 3], 'data2': np.random.randint(0, 6, 6)})
 print(f"{df=}")
 
-print(f"{df.groupby('key').et_group('A')=}")
+print(f"{df.groupby('key').get_group('A')=}")
