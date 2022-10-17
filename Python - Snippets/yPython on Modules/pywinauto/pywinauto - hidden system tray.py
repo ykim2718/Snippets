82a88036d5c98e5f0fd56c555e89cabc39ffb73e
Y <ykim7389@naver.com>
2022-10-18 05:55:12 +0900

    y desktop

--- Python - Snippets/yPython on Modules/pywinauto/pywinauto - hidden system tray.py
+++ Python - Snippets/yPython on Modules/pywinauto/pywinauto - hidden system tray.py
@@ -1,5 +1,5 @@
 """
-y, 2021.1.26, ~2.19; 2022.10.16 - 17
+y, 2021.1.26, ~2.19; 2022.10.16 - 18
 pywinauto - hidden system tray.py.py
 https://stackoverflow.com/questions/50464576/how-to-get-specific-system-tray-icon-using-pywinauto
 https://pywinauto.readthedocs.io/en/latest/HowTo.html?highlight=context%20menu#how-to-access-the-system-tray-aka-systray-aka-notification-area
@@ -14,7 +14,7 @@ import time
 
 print(f"{pwa.__version__=}")
 
-case = 3
+case = 4
 
 if case == 1:  # NOT WORKING
     # https://pywinauto.readthedocs.io/en/latest/HowTo.html#how-to-access-the-system-tray-aka-systray-aka-notification-area
@@ -26,9 +26,16 @@ if case == 1:  # NOT WORKING
     systray_icons.ClickHiddenSystemTrayIcon(0)
     # systray_icons.ClickHiddenSystemTrayIcon('Robo3T 1.4.0', exact=True, double=True).click()
 elif case == 2:  # WORKING at office >> NOT at home 2022.10.17
+    # https://stackoverflow.com/questions/72905986/pywinauto-how-to-right-click-in-systemtray-select-an-item-then-subitem
+    # https://pywinauto.readthedocs.io/en/latest/code/pywinauto.findwindows.html  ★★★
     d = pwa.Desktop(backend='uia')
-    # d['작업 표시줄'].dump_tree()  # << working
-    d['작업 표시줄'].child_window(title='알림 펼침').click()
+    for window in d.windows():
+        print(f"{window.window_text()=}")
+    if False:  # working at office, not at home, 2022.10.18
+        d['작업 표시줄'].dump_tree()
+        d['작업 표시줄'].child_window(title='알림 펼침').click()
+    else:  # working at home as well as headless mode, 2022.10.18 ^__^;;
+        d.window(title='작업 표시줄', top_level_only=False, visible_only=False).child_window(title='알림 펼침').click()
 elif case == 3:  # WORKING at office >> NOT at home 2022.10.16
     # https://stackoverflow.com/questions/62190055/handling-context-menu-of-the-taskbar-icon-with-pywinauto
     app = pwa.Application(backend="uia").connect(path="explorer.exe")
@@ -36,11 +43,11 @@ elif case == 3:  # WORKING at office >> NOT at home 2022.10.16
     # sti.child_window(title="Notification Chevron").wrapper_object().click()  # 갈매기형 펼침 단추
     sti.child_window(title="알림 펼침").wrapper_object().click()  # 갈매기형 펼침 단추  << working
     time.sleep(1)
-    list_box = pwa.Application(backend="uia").connect(class_name="NotifyIconOverflowWindow")  # 오버플로 알림 영역
-    list_box_win = list_box.window(class_name="NotifyIconOverflowWindow")
-    list_box_win.wait('visible', timeout=30, retry_interval=3)
-    # list_box_win.dump_tree()  # << working
-    app_icon = list_box_win.child_window(title='Realtek HD 오디오 관리자')
+    app = pwa.Application(backend="uia").connect(class_name="NotifyIconOverflowWindow")  # 오버플로 알림 영역
+    app_icons = app.window(class_name="NotifyIconOverflowWindow")
+    app_icons.wait('visible', timeout=30, retry_interval=3)
+    # app_icons.dump_tree()  # << working
+    app_icon = app_icons.child_window(title='Realtek HD 오디오 관리자')
     if False:  # working in headful model, not in headless mode
         app_icon.click_input(button='right')
         app = pwa.Application(backend='uia').connect(path=r'C:\Program Files\Realtek\Audio\HDA\RAVCpl64.exe')
@@ -62,12 +69,70 @@ elif case == 3:  # WORKING at office >> NOT at home 2022.10.16
         app['Menu']['사운드 관리자'].click_input()  # .click() is not working
 elif case == 4:  # SHOULD BE WORKING !!!!
     app = pwa.Application(backend="uia").connect(path="explorer.exe")
-    sti = app.window(class_name="Shell_TrayWnd")
-    sti.child_window(title="알림 펼침").wrapper_object().click()  # 갈매기형 펼침 단추  << working
-    time.sleep(1)
-    list_box = pwa.Application(backend="uia").connect(class_name="NotifyIconOverflowWindow")  # 오버플로 알림 영역
-    list_box_win = list_box.window(class_name="NotifyIconOverflowWindow")
-    list_box_win.wait('visible', timeout=30, retry_interval=3)
-    list_box_win.dump_tree()  # << working
-    app_icon = list_box_win.child_window(title='CreonPlus Start')
+    for window in app.windows():
+        print(f"{window.window_text()=}")
+    if False:  # working
+        app.window(title='작업 표시줄', top_level_only=False, visible_only=False).child_window(title='알림 펼침').click()
+        for window in app.windows():
+            print(f"{window.window_text()=}")
+    else:  # stable working
+        w = app.window(title='작업 표시줄', top_level_only=False, visible_only=False).child_window(title='알림 펼침')
+        w.click()
+        w.wait('visible', timeout=30, retry_interval=3)
+    print('NotifyIconOverflowWindow'.ljust(32, '-'))
+    app = pwa.Application(backend="uia").connect(class_name="NotifyIconOverflowWindow")  # 오버플로 알림 영역
+    app_icons = app.window(class_name="NotifyIconOverflowWindow")
+    app_icons.wait('visible', timeout=30, retry_interval=3)
+    print(81)
+    app_icons.dump_tree()  # << working; informative to get hovering text
+    """
+    Control Identifiers:
+
+    Pane - '알림 오버플로'    (L1596, T852, R1734, B990)
+    ['Pane', '알림 오버플로Pane', '알림 오버플로']
+    child_window(title="알림 오버플로", control_type="Pane")
+       | 
+       | Toolbar - '오버플로 알림 영역'    (L1596, T852, R1734, B990)
+       | ['Toolbar', '오버플로 알림 영역Toolbar', '오버플로 알림 영역']
+       | child_window(title="오버플로 알림 영역", auto_id="1504", control_type="ToolBar")
+       |    | 
+       |    | Button - 'AhnLab Online Security 실행 중'    (L1596, T852, R1642, B898)
+       |    | ['Button', 'AhnLab Online Security 실행 중', 'AhnLab Online Security 실행 중Button', 'Button0', 'Button1']
+       |    | child_window(title="AhnLab Online Security 실행 중", control_type="Button")
+       |    | 
+       |    | Button - 'Windows 보안 - 작업이 권장됩니다.'    (L1642, T852, R1688, B898)
+       |    | ['Button2', 'Windows 보안 - 작업이 권장됩니다.Button', 'Windows 보안 - 작업이 권장됩니다.']
+       |    | child_window(title="Windows 보안 - 작업이 권장됩니다.", auto_id="{BA82E2DC-F405-47AD-B032-CF0FAA0E3933}", control_type="Button")
+       |    | 
+       |    | Button - 'AhnLab V3 Lite (엔진 버전: 2022.10.18.00)\r- PC 실시간 검사가 실행 중입니다.'    (L1688, T852, R1734, B898)
+       |    | ['Button3', 'AhnLab V3 Lite (엔진 버전: 2022.10.18.00)\r- PC 실시간 검사가 실행 중입니다.Button', 'AhnLab V3 Lite (엔진 버전: 2022.10.18.00)\r- PC 실시간 검사가 실행 중입니다.']
+       |    | child_window(title="AhnLab V3 Lite (엔진 버전: 2022.10.18.00)\r- PC 실시간 검사가 실행 중입니다.", control_type="Button")
+       |    | 
+       |    | Button - ''    (L1596, T898, R1642, B944)
+       |    | ['Button4']
+       |    | 
+       |    | Button - 'Java Update 사용 가능'    (L1642, T898, R1688, B944)
+       |    | ['Button5', 'Java Update 사용 가능', 'Java Update 사용 가능Button']
+       |    | child_window(title="Java Update 사용 가능", control_type="Button")
+       |    | 
+       |    | Button - '이베스트투자증권(버전확인필수)_20210928'    (L1688, T898, R1734, B944)
+       |    | ['Button6', '이베스트투자증권(버전확인필수)_20210928Button', '이베스트투자증권(버전확인필수)_20210928', '이베스트투자증권(버전확인필수)_20210928Button0', '이베스트투자증권(버전확인필수)_20210928Button1', '이베스트투자증권(버전확인필수)_202109280', '이베스트투자증권(버전확인필수)_202109281']
+       |    | child_window(title="이베스트투자증권(버전확인필수)_20210928", control_type="Button")
+       |    | 
+       |    | Button - '이베스트투자증권(버전확인필수)_20210928'    (L1596, T944, R1642, B990)
+       |    | ['Button7', '이베스트투자증권(버전확인필수)_20210928Button2', '이베스트투자증권(버전확인필수)_202109282']
+       |    | child_window(title="이베스트투자증권(버전확인필수)_20210928", control_type="Button")
+       |    | 
+       |    | Button - 'NVIDIA GeForce Game Ready 드라이버 업데이트를 사용할 수 있습니다'    (L1642, T944, R1688, B990)
+       |    | ['Button8', 'NVIDIA GeForce Game Ready 드라이버 업데이트를 사용할 수 있습니다Button', 'NVIDIA GeForce Game Ready 드라이버 업데이트를 사용할 수 있습니다']
+       |    | child_window(title="NVIDIA GeForce Game Ready 드라이버 업데이트를 사용할 수 있습니다", control_type="Button")
+       |    | 
+       |    | Button - '2022.6.56 버전으로 업그레이드 할 수 있습니다.'    (L1688, T944, R1734, B990)
+       |    | ['Button9', '2022.6.56 버전으로 업그레이드 할 수 있습니다.Button', '2022.6.56 버전으로 업그레이드 할 수 있습니다.']
+       |    | child_window(title="2022.6.56 버전으로 업그레이드 할 수 있습니다.", control_type="Button"
+    """
+    print('CreonPlus Start'.ljust(32, '-'))
+    # app_icon = app_icons.child_window(title='CreonPlus Start')
+    # app_icon = app_icons.button  # working at home, 2022.10.18
+    app_icon = app_icons.child_window(title='Java Update 사용 가능')  # workin at home, 2022.10.18
     app_icon.double_click_input(button='left')  # working only in headful mode, 2022.10.17
