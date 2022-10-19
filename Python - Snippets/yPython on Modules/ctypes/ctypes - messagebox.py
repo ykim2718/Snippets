27d8433a259193047e472f672681a26eb973a4eb
Y <ykim7389@naver.com>
2022-10-19 23:27:58 +0900

    y desktop

---
+++ Python - Snippets/yPython on Modules/ctypes/ctypes - messagebox.py
@@ -0,0 +1,16 @@
+"""
+y, 2022.10.19
+ctypes - messagebox.py
+https://docs.python.org/ko/3/library/ctypes.htmll
+"""
+
+from ctypes import c_int, WINFUNCTYPE, windll
+from ctypes.wintypes import HWND, LPCWSTR, UINT
+
+prototype = WINFUNCTYPE(c_int, HWND, LPCWSTR, LPCWSTR, UINT)
+paramflags = (1, "hwnd", 0), (1, "text", "Hi"), (1, "caption", "Hello from ctypes"), (1, "flags", 0)
+MessageBox = prototype(("MessageBoxW", windll.user32), paramflags)
+
+# MessageBox()
+# MessageBox(text="Spam, spam, spam")
+MessageBox(flags=2, text="foo bar")
