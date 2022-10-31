17d6948109977163c36a76fb1a130d77c26e8263
Y <ykim7389@naver.com>
2022-10-31 18:09:01 +0900

    y desktop

--- Python - Snippets/yPython on Modules/apscheduler/apscheduler - time range.py
+++ Python - Snippets/yPython on Modules/apscheduler/apscheduler - time range.py
@@ -11,14 +11,16 @@ import time
 
 def my_function(*args):
     print(time.ctime())
+    time.sleep(60*5)
 
 
 tz = pytz.timezone('Asia/Seoul')
 bs = BlockingScheduler(timezone=tz)
 
 next_run_time = dt.datetime.now(tz=tz)
-# bs.add_job(my_function, 'cron', hour='0-23', minute='*')  # every minute, same as minute='0-59'
-bs.add_job(my_function, 'cron', hour='0-23')  # every hour, same as minute=None
+bs.add_job(my_function, 'cron', hour='0-23', minute='*')  # every minute, same as minute='0-59'
+# Execution of job "my_function (trigger: cron[hour='0-23', minute='*'], next run at: 2022-10-31 16:29:00 KST)" skipped: maximum number of running instances reached (1)
+# bs.add_job(my_function, 'cron', hour='0-23')  # every hour, same as minute=None
 
 bs.print_jobs()
 for job in bs.get_jobs():
