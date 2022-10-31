fe2a7fa19b9515b7ccfe163c620c536441bff7f0
Y <ykim7389@naver.com>
2022-10-31 16:00:27 +0900

    y desktop

--- Python - Snippets/yPython on Modules/apscheduler/apscheduler - time range.py
+++ Python - Snippets/yPython on Modules/apscheduler/apscheduler - time range.py
@@ -1,5 +1,5 @@
 """
-y, 2022.10.30
+y, 2022.10.30 - 31
 apscheduler - time range.py
 """
 
@@ -10,15 +10,15 @@ import time
 
 
 def my_function(*args):
-    print(ime.ctime())
+    print(time.ctime())
 
 
 tz = pytz.timezone('Asia/Seoul')
 bs = BlockingScheduler(timezone=tz)
 
 next_run_time = dt.datetime.now(tz=tz)
-# next_run_time = None
-bs.add_job(my_function, 'cron', hour='0-23', minute='*')  # same as minute='0-59'
+# bs.add_job(my_function, 'cron', hour='0-23', minute='*')  # every minute, same as minute='0-59'
+bs.add_job(my_function, 'cron', hour='0-23')  # every hour, same as minute=None
 
 bs.print_jobs()
 for job in bs.get_jobs():
