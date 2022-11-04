a8e861eb6adaa09db1b61196a5da338a7423be35
Y <ykim7389@naver.com>
2022-11-04 10:04:26 +0900

    y desktop

--- Python - Snippets/yPython on Modules/typing/typing - overload.py
+++ Python - Snippets/yPython on Modules/typing/typing - overload.py
@@ -1,9 +1,13 @@
 """
-y, 2022.6.18
+y, 2022.6.18, 11.4
 typing - overload.py
 https://docs.python.org/3.9/library/typing.html#typing.overload
 https://peps.python.org/pep-0484/#function-method-overloading
 https://stackoverflow.com/questions/52034771/overload-decorator-in-typings-module-doesnt-seem-to-behave-as-expected
+https://docs.python.org/3/library/typing.html
+    The @overload-decorated definitions are for the benefit of the type checker only,
+    since they will be overwritten by the non-@overload-decorated definition,
+    while the latter is used at runtime but should be ignored by a type checker.
 """
 
 import typing
@@ -13,22 +17,16 @@ class Klass:
 
     @typing.overload
     def __init__(self, a: int) -> None:
-        print(a)
+        print('int', a)
 
     @typing.overload
     def __init__(self, a: str) -> None:
-        print(a)
+        print('str', a)
+
+    def __init__(self, a):
+        print('actual', a)
 
 
-Klass()
+Klass(123)
+Klass('abc')
 
-"""
-Traceback (most recent call last):
-  File "D:\Wolf\Homo academicus\yStudy on code - Python\Python - Snippets\yPython on Modules\typing\typing - overload.py", line 21, in <module>
-    Klass()
-  File "C:\y\Anaconda3\lib\typing.py", line 1582, in _overload_dummy
-    raise NotImplementedError(
-NotImplementedError: You should not call an overloaded function. A series of @overload-decorated functions outside a stub module should always be followed by an implementation that is not @overload-ed.
-
-Process finished with exit code 1
-"""
