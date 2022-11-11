4420686c008abb81d99a6b649f09e9039af64939
Y <ykim7389@naver.com>
2022-11-11 22:42:02 +0900

    y desktop

--- Python - Snippets/yPython on Modules/apscheduler/apscheduler - time range.py
+++ Python - Snippets/yPython on Modules/apscheduler/apscheduler - time range.py
@@ -1,5 +1,5 @@
 """
-y, 2022.10.30 - 31
+y, 2022.10.30 - 31, 11.11
 apscheduler - time range.py
 """
 
@@ -9,9 +9,10 @@ import pytz
 import time
 
 
-def my_function(*args):
-    print(time.ctime())
-    time.sleep(60*5)
+def my_function(*args, _count=[0]):
+    _count[0] += 1
+    print(f"{_count[0]=}", time.ctime())
+    time.sleep(60*2)
 
 
 tz = pytz.timezone('Asia/Seoul')
@@ -19,7 +20,17 @@ bs = BlockingScheduler(timezone=tz)
 
 next_run_time = dt.datetime.now(tz=tz)
 bs.add_job(my_function, 'cron', hour='0-23', minute='*')  # every minute, same as minute='0-59'
-# Execution of job "my_function (trigger: cron[hour='0-23', minute='*'], next run at: 2022-10-31 16:29:00 KST)" skipped: maximum number of running instances reached (1)
+"""
+_count[0]=1 Fri Nov 11 22:07:00 2022
+Execution of job "my_function (trigger: cron[hour='0-23', minute='*'], next run at: 2022-11-11 22:08:00 KST)" skipped: maximum number of running instances reached (1)
+Execution of job "my_function (trigger: cron[hour='0-23', minute='*'], next run at: 2022-11-11 22:09:00 KST)" skipped: maximum number of running instances reached (1)
+_count[0]=2 Fri Nov 11 22:10:00 2022
+Execution of job "my_function (trigger: cron[hour='0-23', minute='*'], next run at: 2022-11-11 22:11:00 KST)" skipped: maximum number of running instances reached (1)
+_count[0]=3 Fri Nov 11 22:12:00 2022
+Execution of job "my_function (trigger: cron[hour='0-23', minute='*'], next run at: 2022-11-11 22:13:00 KST)" skipped: maximum number of running instances reached (1)
+Execution of job "my_function (trigger: cron[hour='0-23', minute='*'], next run at: 2022-11-11 22:14:00 KST)" skipped: maximum number of running instances reached (1)
+_count[0]=4 Fri Nov 11 22:15:00 2022
+"""
 # bs.add_job(my_function, 'cron', hour='0-23')  # every hour, same as minute=None
 
 bs.print_jobs()
