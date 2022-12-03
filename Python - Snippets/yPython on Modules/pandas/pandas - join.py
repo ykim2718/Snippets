3130f7f047ed94a6d6a6e5f963d335c6d5607100
Y <ykim7389@naver.com>
2022-12-04 02:06:53 +0900

    y desktop

--- Python - Snippets/yPython on Modules/pandas/pandas - join.py
+++ Python - Snippets/yPython on Modules/pandas/pandas - join.py
@@ -1,27 +1,20 @@
 """
-y, 2016.7.16 - 7.25, pandas - join.py
+y, 2022.12.3, pandas - join.py
 """
+
 import pandas as pd
 
 df = pd.DataFrame({'c1': [1, 2, 3]}, index=['r1', 'r2', 'r3'])
-dg = pd.DataFrame({'c2': [4, 5, 6]}, index=['r1', 'r2', 'r3'])
-dh = pd.DataFrame({'c3': [7, 8, 9]}, index=['r1', 'r2', 'r3'])
-di = pd.DataFrame({'c4': ['a', 'b', 'c']}, index=df.index)
-dj = pd.DataFrame(dict(c5=['d', 'e']))
-dk = pd.DataFrame(dict(c4=['f']), index=['r1'])
+dg = pd.DataFrame({'c2': [4, 5, 6]}, index=['r3', 'r4', 'r5'])
 
-dx = pd.concat([df, dg, dh, di, dj, dk], axis=1)
-print(dx)
+f = df.join(dg, how='outer')
+print(f)
 
-dy = pd.concat([df, dg, dh, di, dj, dk], axis=0)
-print(dy)
-
-print('y, 2016.7.20, Combine tow columns of text in dataframe in pandas/python')
-df = pd.concat([df, dg, dh, di], axis=1)
-df['c1+c2'] = df[['c1', 'c2']].apply(lambda x: '{},{}'.format(x[0], x[1]), axis=1)
-df['c1+c3'] = df['c1'].map(str) + ',' + df['c3'].map(str)
-print(df)
-
-print('y, 2016.7.25, Add a column from a list')
-df['series'] = dj['c5'].values.tolist() + [None] * (df.shape[0] - len(dj['c5']))
-print(df)
+"""
+     c1   c2
+r1  1.0  NaN
+r2  2.0  NaN
+r3  3.0  4.0
+r4  NaN  5.0
+r5  NaN  6.0
+"""
