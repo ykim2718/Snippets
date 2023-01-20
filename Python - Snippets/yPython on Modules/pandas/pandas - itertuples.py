bd586d91d7b138891bf258e23e6b0c6cff2869b5
Y <ykim7389@naver.com>
2023-01-20 19:24:21 +0900

    y desktop

--- Python - Snippets/yPython on Modules/pandas/pandas - itertuples.py
+++ Python - Snippets/yPython on Modules/pandas/pandas - itertuples.py
@@ -1,5 +1,5 @@
 """
-y, 2022.3.4
+y, 2022.3.4, 1.20
 pandas - itertuples.py
 https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.itertuples.html
 """
@@ -9,7 +9,7 @@ import pandas as pd
 df = pd.DataFrame({'num_legs': [4, 2], 'num_wings': [0, 2]}, index=['dog', 'hawk'])
 
 for row in df.itertuples(index=False):
-    print(row, row.Index, row.num_legs, row.num_wings)
+    print(row, row.num_legs, row.num_wings)
 
 for row in df.itertuples(name='Animal'):
-    print(row)
+    print(row, row.Index, row.num_legs, row.num_wings)
