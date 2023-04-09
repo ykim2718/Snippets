8b0d4494ae191304ea61115e475cc549b9f8b51c
Y <ykim7389@naver.com>
2023-04-09 23:20:47 +0900

    y desktop: 2023.4.9

--- Python - Snippets/yPython on Modules/click/click - list argument.py
+++ Python - Snippets/yPython on Modules/click/click - list argument.py
@@ -2,6 +2,7 @@
 y, 2023.4.9
 click - list argument.py
 https://stackoverflow.com/questions/47631914/how-to-pass-several-list-of-arguments-to-click-option
+https://docs.python.org/3/library/ast.html
 """
 
 import click
@@ -18,13 +19,13 @@ class PythonLiteralOption(click.Option):
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('--option1', cls=PythonLiteralOption, default=[])
-@click.option('--option2', cls=PythonLiteralOption, default=[])
-def cli(option1, option2):
-    click.echo("Option 1, type: {}  value: {}".format(
-        type(option1), option1))
-    click.echo("Option 2, type: {}  value: {}".format(
-        type(option2), option2))
+@click.option('--option1', cls=PythonLiteralOption, default='[1]')
+@click.option('--option2', cls=PythonLiteralOption, default='[1, 2]')
+@click.option('--option3', cls=PythonLiteralOption, default='[1, 2, 3]')
+def cli(option1, option2, option3):
+    click.echo("Option 1, type: {}  value: {}".format(type(option1), option1))
+    click.echo("Option 2, type: {}  value: {}".format(type(option2), option2))
+    click.echo("Option 3, type: {}  value: {}".format(type(option3), option3))
 
 
 # do stuff
@@ -34,5 +35,6 @@ if __name__ == '__main__':
     a = '''
         --option1 '["o11", "o12", "o13"]' 
         --option2 "['o21', 'o22', 'o23']"
+        --option3 '[1, 2, 3, 4]'
     '''
     cli(shlex.split(a))
