e98174f9d0bdb46fc47db7464b3f28ad5b5f0d7c
Y <ykim7389@naver.com>
2023-06-13 01:09:08 +0900

    y desktop: 2023.6.13

--- Python - Snippets/yPython on Modules/pandas/pandas - wide_to_long.py
+++ Python - Snippets/yPython on Modules/pandas/pandas - wide_to_long.py
@@ -7,13 +7,24 @@ import pandas as pd
 import numpy as np
 
 np.random.seed(123)
-df = pd.DataFrame({"A1970" : {0 : "a", 1 : "b", 2 : "c"},
-                   "A1980" : {0 : "d", 1 : "e", 2 : "f"},
-                   "B1970" : {0 : 2.5, 1 : 1.2, 2 : .7},
-                   "B1980" : {0 : 3.2, 1 : 1.3, 2 : .1},
-                   "X"     : dict(zip(range(3), np.random.randn(3)))
-                  })
+df = pd.DataFrame({
+    "A1970": {0: "a", 1: "b", 2: "c"},
+    "A1980": {0: "d", 1: "e", 2: "f"},
+    "B1970": {0: 2.5, 1: 1.2, 2: .7},
+    "B1980": {0: 3.2, 1: 1.3, 2: .1},
+    "X": dict(zip(range(3), np.random.randn(3)))
+})
 df["id"] = df.index
 print(df)
-df = pd.wide_to_long(df, ["A", "B"], i="id", j="year")
+df = pd.wide_to_long(df, stubnames=["A", "B"], i="id", j="year")
+print(df)
+
+print('-' * 32)
+
+df = pd.concat([
+    pd.DataFrame({"A": {0: "a", 1: "b", 2: "c"}, "B": {0: 0, 1: 1, 2: 2}}),
+    pd.DataFrame({"A": {0: "d", 1: "e", 2: "f"}, "B": {0: 3, 1: 4, 2: 5}})
+], axis=1, keys=['f1', 'f2'])
+print(df)
+df = df.stack(0)
 print(df)
