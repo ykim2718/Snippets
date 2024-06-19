1a3cf91db518bb186160a783e14b7ce5439f7098
ykim <ykim2718@gmail.com>
2024-06-19 11:08:55 -0400

    krx is not working

--- Python - Snippets/yPython on Python Grammar/class/python class - __new__.py
+++ Python - Snippets/yPython on Python Grammar/class/python class - __new__.py
@@ -1,5 +1,6 @@
 """
-y, python class - __new__.py, 2019.9.12
+y, 2019.9.12; 2024.6.19
+python class - __new__.py
 https://corikachu.github.io/articles/python/python-magic-method
 https://www.geeksforgeeks.org/dunder-magic-methods-python/
 """
@@ -8,19 +9,22 @@ https://www.geeksforgeeks.org/dunder-magic-methods-python/
 class Klass:
 
     def __new__(self, *args, **kwargs):
-
+        print('called __new__()')
         if len(args) == 0:
             return None
         else:
-            print(args, kwargs, '<<< in Klass.__new__()')
+            print(f"\t{args=}, {kwargs=}, <<< in Klass.__new__()")
             return super().__new__(self)
 
     def __init__(self, x=None):
+        print('called __init__()')
         self.x = x
 
 
 g = Klass()
-print(type(g))
+print(f"\t{type(g)=}")
+
+print('-' * 32)
 
 g = Klass(3)
-print(type(g), g.x)
+print(f"\t{type(g)=}, {g.x=}")
