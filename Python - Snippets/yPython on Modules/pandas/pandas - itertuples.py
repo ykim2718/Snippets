c37e115e2a5c78b9e771cbaae71ffc06e13a6f93
ykim <ykim2718@gmail.com>
2024-09-23 21:46:36 -0400

    cip yControl.hanguktuja_trading()

--- Python - Snippets/yPython on Modules/pandas/pandas - itertuples.py
+++ Python - Snippets/yPython on Modules/pandas/pandas - itertuples.py
@@ -1,15 +1,18 @@
 """
-y, 2022.3.4, 1.20
+y, 2022.3.4, 1.20; 2024.9.22
 pandas - itertuples.py
 https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.itertuples.html
 """
 
 import pandas as pd
 
-df = pd.DataFrame({'num_legs': [4, 2], 'num_wings': [0, 2]}, index=['dog', 'hawk'])
+df = pd.DataFrame({'num_legs': [4, 2], 'num_wings': ['zero', 'two']}, index=['dog', 'hawk'])
 
 for row in df.itertuples(index=False):
     print(row, row.num_legs, row.num_wings)
 
 for row in df.itertuples(name='Animal'):
     print(row, row.Index, row.num_legs, row.num_wings)
+
+for index, num_legs, num_wings in df.itertuples():
+    print(index, num_legs, num_wings)
