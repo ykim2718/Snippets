4f70f4b84ee31601e4ba2e2ebb07c965e2ec2756
ykim <ykim2718@gmail.com>
2026-04-15 13:41:23 -0500

    fe.py

--- Python - Snippets/yPython on Modules/pprint/pprint - basic.py
+++ Python - Snippets/yPython on Modules/pprint/pprint - basic.py
@@ -1,11 +1,20 @@
 """
-y, 2021.5.2
+y, 2021.5.2; 2026.4.15
 pprint - basic.py
 """
 
 import pprint
+from copy import deepcopy
 
-a = [f"abc_{j}" for j in range(33)]
-p = pprint.pformat(a, indent=1, width=80, compact=True)
+d = {
+    'a': 1,
+    'b': dict(a=2, b=3),
+    'c': [f"abc_{j}" for j in range(33)]
+}
+d['d'] = deepcopy(d)
+p = pprint.pformat(d, indent=2, width=80, compact=True)
 print(p)
-print('\t' + p.replace('\n', '\n\t'))
+print(f"{type(p)=}")
+
+# pprint로 출력된 결과물은 json.loads로 직접 읽을 수 없습니다.
+# 만약 pprint로 출력된 (파이썬 형태의) 문자열을 다시 파이썬 객체로 변환하고 싶다면 json.loads 대신 **ast.literal_eval**을 사용해야 합니다.
